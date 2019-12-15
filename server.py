2cf4ff96335315d7faff16fcf2bb2ffb5a6867d1
talhacomak <tcomak32@outlook.com>
2019-12-15 12:31:37 +0300

    tables recreated

--- server.py
+++ server.py
@@ -16,33 +16,6 @@ age = name = weight = height = "-"
 aller = med_dev = surge = medi = discomp = family_diseases = list()
 exam = "no_date"
 
-INIT_STATEMENTS = [
-"INSERT INTO BLOOD_TYPE VALUES(1, 'AB', '+')",
-"INSERT INTO BLOOD_TYPE VALUES(2, 'AB', '-')",
-"INSERT INTO BLOOD_TYPE VALUES(3, '0', '+')",
-"INSERT INTO BLOOD_TYPE VALUES(4, '0', '-')",
-"INSERT INTO BLOOD_TYPE VALUES(5, 'A', '+')",
-"INSERT INTO BLOOD_TYPE VALUES(6, 'A', '-')",
-"INSERT INTO BLOOD_TYPE VALUES(7, 'B', '+')",
-"INSERT INTO BLOOD_TYPE VALUES(8, 'B', '-')",
-"INSERT INTO PATIENT VALUES(1, 'ALI', 13, 50.5, 165, '20.12.2018', 3)",
-"INSERT INTO PATIENT VALUES(2, 'VELI', 12, 45, 130, '05.12.2018', 2)",
-"INSERT INTO MEDICAL_DEVICE VALUES(1, 'GLASSES', 'EYES', 1)",
-"INSERT INTO SURGERY VALUES(1, 'APANDIS', 'KARIN', 2, 1)",
-"INSERT INTO FAMILY_DISEASE VALUES(1, 'DIYABET', 'BOBREK', 1)",
-"INSERT INTO MEDICATION VALUES(1, 'ANTIBIYOTIK', 'SABAH-AKSAM', 1)",
-"INSERT INTO DISCOMFORT VALUES(1, 'BAS AGRISI', 'KAFA', 3, 1)",
-"INSERT INTO USERS VALUES(1, 'ozan', 'ozan', 'ozan@gmail.com', TRUE)",
-"INSERT INTO USERS VALUES(2, 'talha', 'talha', 'talha@gmail.com', FALSE)"
-
-]
-
-with dbapi2.connect(db_url) as connection:
-    cursor = connection.cursor()
-    for statement in INIT_STATEMENTS:
-        cursor.execute(statement)
-    cursor.close()
-
 @app.route("/")
 def home():
     return render_template('home.html')
