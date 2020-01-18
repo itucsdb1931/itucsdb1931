2270eb0f5367089215b99e8a76802da187734fab
talhacomak <tcomak32@outlook.com>
2020-01-18 11:20:09 +0300

    fix

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from passlib.hash import pbkdf2_sha256 as hasher
 import psycopg2 as dbapi2
+import random
 from configurations import db_url
 
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT' + str(random.random())
 INIT_STATEMENTS = [
 ]
 
@@ -1448,5 +1450,4 @@ def add_aller():
                                display_medi="none", display_medi_ad="none", display_medi_del="none", upmedi='n')
 
 if __name__ == "__main__":
-    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run()
